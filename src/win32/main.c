120770a8fdb7948d3ce7641b9808346d0cd28637
wasabi2710 <em.vannin.2001@gmail.com>
2025-02-26 21:07:51 +0700

    wnd & lin native support done

--- src/win32/main.c
+++ src/win32/main.c
@@ -1,57 +1,96 @@
-#include <SDL3/SDL.h>
+#include <windows.h>
 #include <stdio.h>
 
-/*init window and renderer*/
-SDL_Renderer* renderer;
-SDL_Window* window;
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
 
-void cleanup() {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+            // Draw semi-transparent red circle
+            HBRUSH brush = CreateSolidBrush(RGB(255, 0, 0));  // Red color
+            Ellipse(hdc, 150, 150, 250, 250);  // Draw circle in the center
+
+            // Draw horizontal bars
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+            int barHeight = 20;
+            int spacing = 10;
+
+            // Draw bars above circle
+            for(int y = 50; y < 150; y += barHeight + spacing) {
+                Rectangle(hdc, 100, y, 600, y + barHeight);
+            }
+
+            // Draw bars below circle
+            for(int y = 250; y < rect.bottom - 50; y += barHeight + spacing) {
+                Rectangle(hdc, 100, y, 600, y + barHeight);
+            }
+
+            DeleteObject(brush);
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+        case WM_LBUTTONDOWN: {
+            // Get the coordinates of the click
+            int xPos = LOWORD(lParam);
+            int yPos = HIWORD(lParam);
+
+            // Check if the click is inside the circle
+            int circleCenterX = 200;
+            int circleCenterY = 200;
+            int radius = 50;
+
+            int dx = xPos - circleCenterX;
+            int dy = yPos - circleCenterY;
+            if (dx * dx + dy * dy <= radius * radius) {
+                // Print "Hello" if the click is inside the circle
+                MessageBox(hwnd, "Hello", "Info", MB_OK);
+            }
+            return 0;
+        }
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 int main() {
-    /*Check sdl vid init*/
-    if(!SDL_Init(SDL_INIT_VIDEO)) {
-        // fprintf(stderr, "%s", "Failed: sdl_init_video");
-        printf("Failed Video Initialize\n");
-        return 1;
+    const char CLASS_NAME[] = "Transparent Window Class";
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = CLASS_NAME;
+    if (!RegisterClass(&wc)) {
+        return 0;
     }
 
-    if (!SDL_CreateWindowAndRenderer("KittyPulse", 400, 400, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
-        printf("SDL Window Creation failed\n");
-        SDL_Quit();
-        return 1;
+    HWND hwnd = CreateWindowEx(
+        WS_EX_LAYERED,  // Extended style for transparency
+        CLASS_NAME, "Transparent Window",
+        WS_POPUP,
+        100, 100, 800, 600,
+        NULL, NULL, wc.hInstance, NULL
+    );
+
+    if (!hwnd) {
+        return 0;
     }
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    // Set semi-transparency (alpha: 200 = less transparent)
+    SetLayeredWindowAttributes(hwnd, 0, 200, LWA_ALPHA);
 
-    /*event poll*/
-    SDL_Event e;
+    HRGN hRegion = CreateEllipticRgn(150, 150, 250, 250);
+    SetWindowRgn(hwnd, hRegion, TRUE);
 
-    while(1) {
-        if (SDL_PollEvent(&e)) {
-            if (e.type == SDL_EVENT_QUIT) {
-                cleanup();
-                return SDL_APP_SUCCESS;
-            }
-            if (e.type == SDL_EVENT_KEY_DOWN) {
-                switch (e.key.scancode) {
-                    case SDL_SCANCODE_W:
-                        rect.x += 1;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderRect(renderer, &rect);
-
-        SDL_RenderPresent(renderer);
-        SDL_Delay(16); /*60fps*/
-    }   
-
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
 }
