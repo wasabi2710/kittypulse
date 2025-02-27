61447977ee1764b79ae773218d01cf243f52e7cf
wasabi2710 <em.vannin.2001@gmail.com>
2025-02-27 12:47:28 +0700

    ignore last msg, active interactable is back

--- src/win32/main.c
+++ src/win32/main.c
@@ -5,24 +5,39 @@
 SDL_Window* window;
 SDL_Renderer* renderer;
 
+void SetClickableRegion(HWND hwnd, int centerX, int centerY, int width, int height) {
+    int left = centerX - (width / 2);
+    int top = centerY - (height / 2);
+    int right = centerX + (width / 2);
+    int bottom = centerY + (height / 2);
+
+    HRGN hRgnClickable = CreateRectRgn(left, top, right, bottom);
+    SetWindowRgn(hwnd, hRgnClickable, TRUE);
+    DeleteObject(hRgnClickable); // Important: Release the region
+}
+
 int main() {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         printf("Failed Init SDL Video\n");
         return 1;
     }
 
-    if (!SDL_CreateWindowAndRenderer("KittyPulse", 500, 500, SDL_WINDOW_TRANSPARENT | SDL_WINDOW_ALWAYS_ON_TOP, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("KittyPulse", 500, 500, SDL_WINDOW_ALWAYS_ON_TOP, &window, &renderer)) {
         printf("Failed Window & Renderer Creation\n");
         return 1;
     }
 
     // set layered
     HWND hwnd = GetActiveWindow();
-    SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+    SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_TRANSPARENT);
     SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
 
+    // Set the clickable region
+    SetClickableRegion(hwnd, 250, 250, 50, 50); // Center of the window, 200x200 clickable area
+
     SDL_Event e;
-    float mouseX, mouseY;
+    POINT cursorPos;
+    SDL_Point clientCursorPos;
 
     while (1) {
         if (SDL_PollEvent(&e) != 0) {
@@ -30,11 +45,14 @@ int main() {
                 printf("Exit program ...\n");
                 return 0; //exit_success
             }
+        }
 
-            if (e.type == SDL_EVENT_MOUSE_MOTION) {
-                // Track the mouse position
-                SDL_GetMouseState(&mouseX, &mouseY);
-                printf("Mouse position: %f, %f\n", mouseX, mouseY);
+        // Get mouse position using Windows API
+        if (GetCursorPos(&cursorPos)) {
+            if (ScreenToClient(hwnd, &cursorPos)) {
+                clientCursorPos.x = cursorPos.x;
+                clientCursorPos.y = cursorPos.y;
+                //printf("Mouse position: %d, %d\n", clientCursorPos.x, clientCursorPos.y);
             }
         }
 
@@ -43,12 +61,8 @@ int main() {
         SDL_RenderClear(renderer);
 
         // Draw the rectangle with a different color (e.g., green)
-        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-        SDL_FRect rect;
-        rect.x = rect.y = 50; // Position the rectangle
-        rect.w = rect.h = 100; // Size of the rectangle
-        SDL_RenderRect(renderer, &rect);
-        
+
+
         SDL_RenderPresent(renderer);
     }
 }
